3993922879619be6461efbd5ab9db77422901137
Federica Macchi <federica.macchi3@gmail.com>
2019-06-09 08:53:05 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        my_word  = @word.split("").sort
+        worked = anagrams.select do |test_word|
+            check_word = test_word.split("").sort
+            if my_word == check_word
+                test_word
+            end
+        end 
+        return worked
+    end
+end
